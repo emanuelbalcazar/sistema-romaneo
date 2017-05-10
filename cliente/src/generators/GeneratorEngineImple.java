d42fa2a5cdc50941c341e18fb0906b2727b6c86c
Emanuel <emanuelbalcazar13@gmail.com>
2017-05-10 18:09:18 -0300

    Correccion al merge del Motor de generador

--- cliente/src/generators/GeneratorEngineImple.java
+++ cliente/src/generators/GeneratorEngineImple.java
@@ -3,40 +3,31 @@ package generators;
 import administrator.QueueManagement;
 
 /**
- * Implementacion del motor de generacion de mensajes.
- * Se encarga de instanciar los generadores de mensajes disponibles e inicia
- * su ejecucion.
+ * Implementacion del motor de generacion de mensajes. Se encarga de instanciar
+ * los generadores de mensajes disponibles e inicia su ejecucion.
  */
 public class GeneratorEngineImple implements GeneratorEngine {
-    
+
     private final QueueManagement queueManagement;
-    
+
     public GeneratorEngineImple(QueueManagement management) {
         this.queueManagement = management;
     }
-    
-    
-   /** @Override
+
+    @Override
     public void startGenerators() {
-        
+
         MessageGeneratorGeneric romaneo = new RomaneoMessageGenerator();
         romaneo.setQueueManagement(queueManagement);
         romaneo.start();
-<<<<<<< Updated upstream
 
         MessageGeneratorGeneric texto = new TextMessageGenerator();
         texto.setQueueManagement(queueManagement);
         texto.start();
-=======
-    }*/
-    
-    @Override
-    public void startGenerators() {
+
         MessageGeneratorGeneric geo = new GeolocationMessageGenerator();
         geo.setQueueManagement(queueManagement);
         geo.start();
->>>>>>> Stashed changes
     }
-    
-    
+
 }
