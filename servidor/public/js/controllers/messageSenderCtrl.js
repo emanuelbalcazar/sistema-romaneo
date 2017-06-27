286e5abbbc9229ed48c1c4ca3b244167af793de7
Nahuel Alamo <apu.n.alamo@gmail.com>
2017-06-27 18:13:54 -0300

    Modificacion envio de msjs desde Servidor
    - En vista agrego para enviar a distintos dispositivos.
    - En controlador, recibo variable de vista y seteo id de msj.

--- servidor/public/js/controllers/messageSenderCtrl.js
+++ servidor/public/js/controllers/messageSenderCtrl.js
@@ -1,58 +1,61 @@
 (function () {
   'use strict';
-    angular.module('controllerModule')
-    .controller('messageSenderCtrl', ['$scope', '$rootScope', '$location','messageSrv', messageSenderCtrl]);
+  angular.module('controllerModule')
+  .controller('messageSenderCtrl', ['$scope', '$rootScope', '$location', 'messageSrv', messageSenderCtrl]);
 
-    // Controlador de la pantalla de mensajes.
-    function messageSenderCtrl($scope, $rootScope, $location,messageSrv) {
+  // Controlador de la pantalla de mensajes.
+  function messageSenderCtrl($scope, $rootScope, $location, messageSrv) {
 
-      $scope.message = "";
-      $scope.showSuccess= false;
-      $scope.showDanger= false;
-      $scope.sendMessage =sendMessage;
-      $scope.resetLayer= resetLayer;
+    var idM = 0;
+    $scope.message = "";
+    $scope.showSuccess = false;
+    $scope.showDanger = false;
+    $scope.sendMessage = sendMessage;
+    $scope.resetLayer = resetLayer;
+    $scope.equipo = 1;
 
-      /**
+    /**
       *Funcion encargada de enviar un Mesaje desde el servidor al Cliente
       *
       */
-      function sendMessage(){
+    function sendMessage() {
 
-        if($scope.message==""){
-          $scope.showDanger=true;
-          $scope.messageDanger="Ingrese el mensaje para el Cliente"
-          return;
-        }
-
-        var formatMessage = {
-          id:1,
-          imei: 1,
-          priority: 4,
-          type: 'TEXTO',
-          subType:'',
-          operation:'',
-          timestamp: new Date(),
-          text: $scope.message
-
-        }
-        messageSrv.senderMsg(formatMessage).then(function(response){
-          console.log(response);
-          $scope.showSuccess=true;
-          $scope.messageSuccess='Mensaje enviado con Exito';
-        });
+      if ($scope.message == "") {
+        $scope.showDanger = true;
+        $scope.messageDanger = "Ingrese el mensaje para el Cliente";
+        return;
       }
 
-      /**
+      var formatMessage = {
+          id: idM,
+          imei: $scope.equipo,
+          priority: 4,
+          type: 'TEXTO',
+          subType: '',
+          operation: '',
+          timestamp: new Date(),
+          text: $scope.message
+        }
+      messageSrv.senderMsg(formatMessage).then(function (response) {
+          console.log(response);
+          $scope.showSuccess = true;
+          $scope.messageSuccess = 'Mensaje enviado con Exito';
+          idM++;
+        });
+    }
+
+    /**
       *
       *
       */
-      function resetLayer(){
+    function resetLayer() {
 
-        $scope.message="";
-        $scope.showSuccess= false;
-        $scope.showDanger= false;
+      $scope.equipo = 1;
+      $scope.message = "";
+      $scope.showSuccess = false;
+      $scope.showDanger = false;
 
-      }
+    }
 
-    } // end messageSenderCtrl
+  } // end messageSenderCtrl
 })();
